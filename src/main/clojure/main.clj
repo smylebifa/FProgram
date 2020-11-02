190c6203325000c62581fc961dc0ba117cd99457
smyle-pro <smaylic111new@yandex.ru>
2020-11-02 12:56:19 +0300

    changing function, by adding predicate and lazy computation

--- src/main/clojure/main.clj
+++ src/main/clojure/main.clj
@@ -1,13 +1,24 @@
-; command include file
-; (require '[clojure.core])
+; Command include file...
+;(require '[clojure.core])
 
-; lazy sequence
+; Create lazy sequence...
 (defn whole-numbers [] (iterate inc 1))
+;(func1 whole-numbers odd? even?)
 
-; function with sequence as param
-; return 2 sequences with condition ( < 5) or ( >= 5)
-(defn func [s]
-  (split-with #(< % 5) (take 15 (s))))
+; Function with sequence, predicate as param
+; return 10 elements of 2 sequences for predicates...
+(defn func1 [seq predicate1 predicate2]
+  (let [a (filter predicate1 (seq))
+        b (filter predicate2 (seq))]
+    (println (take 10 a) (take 10 b))))
 
-; command to start function
-; (func whole-numbers)
+; Command to start function...
+;(func1 whole-numbers odd? even?)
+
+
+; Trying to make function with 1 parameter...
+;(defn func2 [seq predicate]
+;  (let [a (filter predicate (seq))
+;        b (filter even? (seq))]
+;    (println (take 10 a) (take 10 b))))
+;(func2 whole-numbers odd?)
